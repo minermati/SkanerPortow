e3d010e471def7117cbd72116b6fd72b17d742dd
minermati <mateuszszykowiec@gmail.com>
2026-03-14 00:15:56 +0100

    Full Version with web ui and file export

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse  # <--- TO JEST NOWY IMPORT
+
+# Importujemy bazę i nasze moduły
 from database import engine, Base
 from routers import ports, subdomains, directories, history, techstack, dns_scanner
 
+# Tworzy plik bazy danych, jeśli nie istnieje
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Recon API", description="Profesjonalne narzędzie OSINT")
 
-# Zarejestrowane moduły
+# Zarejestrowane moduły (nasze skanery)
 app.include_router(ports.router)
 app.include_router(subdomains.router)
 app.include_router(directories.router)
-app.include_router(techstack.router)      # <--- NOWY
-app.include_router(dns_scanner.router)    # <--- NOWY
+app.include_router(techstack.router)
+app.include_router(dns_scanner.router)
 app.include_router(history.router)
 
+# ========================================================
+# ZMIANA TUTAJ: Zamiast tekstu, zwracamy plik HTML!
+# ========================================================
 @app.get("/")
 def read_root():
-    return {"message": "Recon API gotowe do akcji! Wejdź na /docs"}
+    # Kiedy wejdziesz na http://127.0.0.1:8000/, FastAPI wyśle ten plik:
+    return FileResponse("index.html")
